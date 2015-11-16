c4dfc46a7669fe05e192a7f431b27445df00bb10
william0630 <william0630love@gmail.com>
2015-11-16 10:31:23 -0800

    update apipie-description

--- app/views/api/v1/users/update.json.jbuilder
+++ app/views/api/v1/users/update.json.jbuilder
@@ -1,8 +1,2 @@
 json.success true
 json.status 201
-json.user do
-  json.id @user.id
-  json.email @user.email
-  json.name @user.name
-  json.customer_id @user.customer_id
-end
--- lib/apipie_descriptions/users.rb
+++ lib/apipie_descriptions/users.rb
@@ -31,7 +31,7 @@ module ApipieDescriptions::Users
   end
 
   def apipie_users_update
-    api :POST, "/users", "[PUBLIC] Validate Credit Card."
+    api :PUT, "/users/:id", "[PUBLIC] Validate Credit Card."
     param :card_number, Integer, required:true
     param :expired_month, Integer, required:true
     param :expired_year, Integer, required:true
